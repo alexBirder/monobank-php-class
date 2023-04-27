7a81c58554993c85a18499b60bc03e95bc9adbeb
Alex <qufors@gmail.com>
2023-04-27 19:14:22 +0300

    Create webhook.php

---
+++ webhook.php
@@ -0,0 +1,8 @@
+<?php
+
+$monoData = file_get_contents('php://input');
+$monoArray = json_decode($monoData, true);
+
+// Для логгирования данных
+$jsonData = json_encode($data);
+file_put_contents('mono.log', $jsonData);
