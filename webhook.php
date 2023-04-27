d63feb7c6424625659d900c44e6f32c285e81466
Alex <qufors@gmail.com>
2023-04-27 19:20:04 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -4,5 +4,5 @@ $monoData = file_get_contents('php://input');
 $monoArray = json_decode($monoData, true);
 
 // Для логгирования данных
-$jsonData = json_encode($data);
+$jsonData = json_encode($monoArray);
 file_put_contents('mono.log', $jsonData);
